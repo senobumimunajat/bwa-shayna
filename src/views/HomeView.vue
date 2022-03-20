c8a3a844b82ebec8d8408013d23f9d9072287e81
senobumimunajat <senobumimunajat15@gmail.com>
2022-03-20 22:47:15 +0700

    slicing template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-shayna />
+    <hero-shayna />
+    <item-shayna />
+    <insta-shayna />
+    <partner-shayna />
+    <footer-shayna />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import ItemShayna from "@/components/ItemShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    HeaderShayna,
+    HeroShayna,
+    ItemShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
 </script>
